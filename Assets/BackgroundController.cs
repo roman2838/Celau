bd4e75f2e70353cb8a104983f94692684b55dcb5
roman2838 <gmx@romanbr.de>
2016-12-07 14:27:44 +0100

    Added Skybox and Eyecandy

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+    public GameObject Planet;
+    public float SpeedX, SpeedY, SpeedZ;
+    public float RotX, RotY, RotZ;
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Rigidbody rb = Planet.GetComponent<Rigidbody>;
+        Vector3 old = Planet.transform.position;
+        Planet.transform.position = old + new Vector3(SpeedX, SpeedY, SpeedZ);
+        Planet.transform.Rotate(new Vector3(RotX, RotY, RotZ));
+
+	}
+}
